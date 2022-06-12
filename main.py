88bd63371b6607d298a9d6c6b957edf17f7d18b4
Hazik Chaudhry <100536561+hazu-kan@users.noreply.github.com>
2022-06-12 03:24:16 +0000

    made components fit the design

--- main.py
+++ main.py
@@ -2,42 +2,39 @@ from tkinter import *
 from PIL import Image, ImageTk
 import random 
 from tkinter import messagebox
+import math
 names = []
 global questions_answers
 asked = []
 score=0
 
-class Quiz:
-    def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
- 
-        background_color="white"
+class UserEnterQuiz:
+  def __init__(self, parent):
+      self.heading_label = Label (parent, text = "", font=("", "1", "bold"))
+      self.heading_label.place(x=50,y=230)
       
-        self.heading_label=Label(parent, text="", font=("Tw Cen MT","18","bold"))
-        self.heading_label.grid(row=0) 
-
         #label for username
-        self.user_label=Label(parent, text="", font=("Tw Cen MT","1"),bg=background_color)
-        self.user_label.place(x=50,y=230)
+      self.user_label=Label(parent, text="", font=("","1" ))
+      self.user_label.place(x=45,y=230)
         
         #entry box
-        self.entry_box=Entry(parent)
-        self.entry_box.place(x=225,y=315) 
+      self.entry_box=Entry(parent, font=("Times 20 italic bold","20"))
+      self.entry_box.place(x=175,y=305) 
         
         #continue button
-        self.continue_button = Button(parent, text="Continue", font=("Helvetica", "35", "bold"), bg="light blue", command=self.name_collection)
-        self.continue_button.place(x=208,y=365)       
+      self.continue_button = Button(parent, text="Continue", font=("Helvetica", "35", "bold",), bg="light blue", command=self.name_collection)  
+      self.continue_button.place(x=208,y=365)              
        
-
-
-    def name_collection(self):
+  def name_collection(self):
+    name=self.entry_box.get()
+    names.append(name) #add name to names list declared at the beginning
+    self.heading_label.destroy()
+    self.user_label.destroy()
+    self.entry_box.destroy()
+    self.continue_button.destroy()
+    UserEnterQuiz(root)
     
-     name=self.entry_box.get()
-     names.append(name) #add name to names list declared at the beginning
-     self.heading_label.destroy()
-     self.user_label.destroy()
-     self.entry_box.destroy()
-     self.continue_button.destroy()
-     Quiz(root)  
+      
       #add name to names list declared at the beginning
         #self.quiz_frame.destroy() #Destroy name frame then open the quiz runner
       
@@ -62,6 +59,6 @@ if __name__ == "__main__":
     image_label= Label(root, image=bg_image)
     image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the parent window always
 
-    quiz_instance = Quiz(root) #instantiation, making an instance of the class Quiz
+    quiz_instance = UserEnterQuiz(root) #instantiation, making an instance of the class Quiz
     root.mainloop()#so the frame doesnt dissapear
 
