d9585afc1ef008d51222e17857fec329577ed603
Hazik Chaudhry <100536561+hazu-kan@users.noreply.github.com>
2022-06-21 01:22:51 +0000

    added restart button

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ questions_answers = {
   3: ["Who starrred in the music video Out of Time by the Weeknd", 'HoYeon Jung', 'Lee Jung-jae', 'Jeffrey Su', 'Anupam Tripathi', 'HoYeon Jung',1],
   4: ["How many strings on a violin?", '3', '-17', '0', '4', '4',4],
   5: ["Which show made One Direction", 'The Voice ', 'X Factor', 'Americas Got Talents', 'American Idol', 'X Factor',2],
-  6: ["When did Beyonce debut?", '1997', '2001', '1945', '2015', '1995',4],
+  6: ["When did Beyonce debut?", '1997', '2001', '1995', '2015', '1995',3],
   7: ["What is Drakes most streamed song to date?", 'Gods Plan', 'Hotline Bling', 'One Dance', 'In my Feelings', 'One Dance',3],
   8: ["What year was YMCA realeased?", '1956', '1978', '1996', '1986', '1978',2],
   9: ["How many memebers does BTS have?", '12', '7', '15','78', '7',2],
   10: ["Which mathematical symbol was the title of Ed Sheeran’s first album in 2011?", '+', '-', 'x','=', '+',1],
+  11: ["How old was Mozart when he wrote his first piece?", '5', '17', '15','25', '21',1],
    
 }
 
 def scrambler(): 
    global qnum
-   qnum = random.randint(1,9) 
+   qnum = random.randint(1,11) 
    if qnum not in asked:
       asked.append(qnum)
    elif qnum in asked:
@@ -33,12 +34,12 @@ def scrambler():
 
 class UserEnterQuiz:
   def __init__(self, parent):
-      self.heading_label = Label (parent, text = "THE MUSIZ QUIZ", font=("Helvetica", "35", "bold"))
-      self.heading_label.place(x=175,y=150)
+      self.heading_label = Label (parent, text = "THE MUSIC QUIZ", font=("Helvetica", "35", "bold"))
+      self.heading_label.place(x=150,y=150)
       
         #label for username
       self.user_label=Label(parent, text="Enter Username Here:", font=("Helvetica","15" ))
-      self.user_label.place(x=45,y=230)
+      self.user_label.place(x=200,y=270)
         
         #entry box
       self.entry_box=Entry(parent, font=("Times 20 italic bold","20"))
@@ -124,16 +125,18 @@ class Questions:
     scr_label = self.score_label 
     score_label = self.answer_label
     choice = self.var1.get() 
-    if len(asked)>6: #to determine if its the last question and just end the quiz after
+    if len(asked)>10: #to determine if its the last question and just end the quiz after
       if choice==questions_answers[qnum][6]: 
         score +=1 
         scr_label.configure(text=score) 
         self.confirm_button.config(text="Confirm") 
         score_label.configure(text="")
+        self.endScreen()
       else:
         score+=0 
         score_label.configure(text=" NO!...The correct answer is"+ questions_answers[qnum][5]) 
         self.confirm_button.config(text="Confirm")
+        self.endScreen()
     else:
       if choice==0:#if the user does not select an  option
         self.answer_label.config(text="Please select a given option.") #error message 
@@ -154,14 +157,14 @@ class Questions:
   def endScreen(self):   
     self.title_label.destroy()
     self.question_label.destroy()
-    self.rb1.destroy()
-    self.rb2.destroy()
-    self.rb3.destroy()
-    self.rb4.destroy()
     self.confirm_button.destroy()
     self.score_label.destroy()
     self.answer_label.destroy()
     self.quit.destroy()
+    self.rb1.destroy()
+    self.rb2.destroy()
+    self.rb3.destroy()
+    self.rb4.destroy()
     name=names[0]
     file=open("board.txt","a") #opens the highscores file
     
@@ -185,7 +188,7 @@ class Questions:
     top.reverse()
     return_string = ""
     for i in range(len(top)):
-      return_string +="{} - {}\n".format(top[i][0], top[i][0])
+      return_string +="{} - {}\n".format(top[i][0], top[i][1])
     print(return_string) #for testing to show on the console
     open_endscrn=leaderboard(root)
     open_endscrn.exit_button.config(text=return_string)   
@@ -213,10 +216,17 @@ class leaderboard:
 
     self.exit_button = Button (self.end_frame, text="Exit", width=10, bg="Red", font=('Tw Cen MT', 12 , 'bold'), command=self.close_end )
     self.exit_button.grid(row=4, pady=20)
+    
+    self.restart_button = Button (self.end_frame, text="Restart", width=10, bg="Green", font=('Tw Cen MT', 12 , 'bold'), command=self.restart )
+    self.restart_button.grid(row=4, pady=30)
 
   def close_end(self):
     self.end_box.destroy()
     root.withdraw()
+
+  def restart(self):
+    UserEnterQuiz(root)
+    self.end_box.destroy()
 
 
 
